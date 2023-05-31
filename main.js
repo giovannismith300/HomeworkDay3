8cd9409302ab2b347e5ecef879e18ab27f661aa1
skojr <133845711+skojr@users.noreply.github.com>
2023-05-31 13:25:04 -0400

    Samuel's Function

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ function fibnum(){
 }
 
 fibnum()
+
+// This is Samuel's function
+let a = 0, b = 1, third;
+function fibonacci(iter) {
+    for (let i = 1; i <= iter; i++) {
+    console.log(a);
+    third = a + b;
+    a = b;
+    b = third;
+    }
+}
+
+fibonacci(10);
